eb9c3c1a87d3550c867d418d6b3de2984cb56ef7
Janne Tolppanen <janne.ks@gmail.com>
2023-12-31 09:14:50 +0200

    categories toimii jotenkin

--- src/photos/photos.module.ts
+++ src/photos/photos.module.ts
@@ -4,10 +4,17 @@ import { PhotosService } from './photos.service';
 import { Photo } from './entities/photo.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from 'src/users/users.module';
+import { CategoriesModule } from 'src/categories/categories.module';
+import { Category } from 'src/categories/entities/category.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Photo]), UsersModule],
+  imports: [
+    TypeOrmModule.forFeature([Photo, Category]),
+    UsersModule,
+    CategoriesModule,
+  ],
   controllers: [PhotosController],
   providers: [PhotosService],
+  exports: [PhotosService],
 })
 export class PhotosModule {}
--- src/photos/photos.service.ts
+++ src/photos/photos.service.ts
@@ -5,12 +5,17 @@ import { Repository } from 'typeorm';
 import { CreatePhotoDto } from './dto/create-photo.dto';
 import { UsersService } from 'src/users/users.service';
 import { UpdatePhotoDto } from './dto/update-photo.dto';
+import { CategoriesService } from 'src/categories/categories.service';
+import { Category } from 'src/categories/entities/category.entity';
 
 @Injectable()
 export class PhotosService {
   constructor(
     @InjectRepository(Photo) private photosRepository: Repository<Photo>,
+    @InjectRepository(Category)
+    private categoriesRepository: Repository<Category>,
     private usersService: UsersService,
+    private categoriesService: CategoriesService,
   ) {}
 
   // Search for a user by email address and if found, create a new photo
@@ -25,12 +30,23 @@ export class PhotosService {
     }
     console.log(`insertPhoto user found ${user.username}`);
 
+    const categories = createPhotoDto.categories.map(async (categoryId) => {
+      const category = await this.categoriesService.findCategoryById(
+        parseInt(categoryId),
+      );
+      if (!category) {
+        throw new NotFoundException(`Category not found: ${categoryId}`);
+      }
+      return category;
+    });
+
     // TODO: Add categories to photo item
     const photo = new Photo();
     photo.name = createPhotoDto.name;
     photo.description = createPhotoDto.description;
     photo.url = createPhotoDto.url;
     photo.user = user;
+    photo.categories = await Promise.all(categories);
     return await this.photosRepository.save(photo);
   }
 
